ddc6eb5dbe1e7eeef73146f762f5da0e6c0f7306
kamal heydari <kamal3heydari@gmail.com>
2023-10-06 20:41:03 +0330

    redeploy

--- app.js
+++ app.js
@@ -6,21 +6,7 @@ const routes = require("./routes/index")
 const rateLimit = require("express-rate-limit") // Basic rate-limiting middleware for Express. Use to limit repeated requests to public APIs and/or endpoints such as password reset.
 const helmet = require("helmet") // Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
 
-// These headers are set in response by helmet
 
-// Content-Security-Policy: default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests
-// Cross-Origin-Embedder-Policy: require-corp
-// Cross-Origin-Opener-Policy: same-origin
-// Cross-Origin-Resource-Policy: same-origin
-// Origin-Agent-Cluster: ?1
-// Referrer-Policy: no-referrer
-// Strict-Transport-Security: max-age=15552000; includeSubDomains
-// X-Content-Type-Options: nosniff
-// X-DNS-Prefetch-Control: off
-// X-Download-Options: noopen
-// X-Frame-Options: SAMEORIGIN
-// X-Permitted-Cross-Domain-Policies: none
-// X-XSS-Protection: 0
 
 const mongosanitize = require("express-mongo-sanitize") // This module searches for any keys in objects that begin with a $ sign or contain a ., from req.body, req.query or req.params.
 
